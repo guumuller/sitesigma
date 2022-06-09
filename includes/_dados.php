1b451034188e57dd713763d0709098aada105727
guumuller <92996025+guumuller@users.noreply.github.com>
2022-06-09 12:22:57 -0300

    08.06.2022

--- includes/_dados.php
+++ includes/_dados.php
@@ -2,15 +2,16 @@
 
 $produtos = array(
     0 => array('nome'=>'capivara legal','preco'=>'1.200','descricao'=>'capivara legal','imagem'=>'capivaralegal.jpg'),
-    1 => array('nome'=>'capivara legal','preco'=>'1.200','descricao'=>'capivara legal','imagem'=>'capivaralegal.jpg'),
-    2 => array('nome'=>'capivara legal','preco'=>'1.200','descricao'=>'capivara legal','imagem'=>'capivaralegal.jpg'),
-    3 => array('nome'=>'capivara legal','preco'=>'1.200','descricao'=>'capivara legal','imagem'=>'capivaralegal.jpg'),
-    4 => array('nome'=>'capivara legal','preco'=>'1.200','descricao'=>'capivara legal','imagem'=>'capivaralegal.jpg'),
-    5 => array('nome'=>'capivara legal','preco'=>'1.200','descricao'=>'capivara legal','imagem'=>'capivaralegal.jpg'),
-    6 => array('nome'=>'capivara legal','preco'=>'1.200','descricao'=>'capivara legal','imagem'=>'capivaralegal.jpg'),
-    7 => array('nome'=>'capivara legal','preco'=>'1.200','descricao'=>'capivara legal','imagem'=>'capivaralegal.jpg'),
-    8 => array('nome'=>'capivara legal','preco'=>'1.200','descricao'=>'capivara legal','imagem'=>'capivaralegal.jpg'),
-    9 => array('nome'=>'capivara legal','preco'=>'1.200','descricao'=>'capivara legal','imagem'=>'capivaralegal.jpg'),
+    1 => array('nome'=>'cachorro legal','preco'=>'1.200','descricao'=>'capivara legal','imagem'=>'goldenlegal.jpg'),
+    2 => array('nome'=>'gato legal','preco'=>'1.200','descricao'=>'capivara legal','imagem'=>'gatolegal.jpg'),
+    3 => array('nome'=>'girafa legal','preco'=>'1.200','descricao'=>'capivara legal','imagem'=>'capivaralegal.jpg'),
+    4 => array('nome'=>'elefante legal','preco'=>'1.200','descricao'=>'capivara legal','imagem'=>'capivaralegal.jpg'),
+    5 => array('nome'=>'suricato legal','preco'=>'1.200','descricao'=>'capivara legal','imagem'=>'capivaralegal.jpg'),
+    6 => array('nome'=>'galinha legal','preco'=>'1.200','descricao'=>'capivara legal','imagem'=>'capivaralegal.jpg'),
+    7 => array('nome'=>'baleia legal','preco'=>'1.200','descricao'=>'capivara legal','imagem'=>'capivaralegal.jpg'),
+    8 => array('nome'=>'leao legal','preco'=>'1.200','descricao'=>'capivara legal','imagem'=>'capivaralegal.jpg'),
+    9 => array('nome'=>'pantera legal','preco'=>'1.200','descricao'=>'capivara legal','imagem'=>'capivaralegal.jpg'),
 );
+
 
 ?>
--- index.php
+++ index.php
@@ -6,6 +6,17 @@ include_once './includes/_header.php';
 ?>
 
 <h1>Home</h1>
+<?php
+   for ($i=0; $i < 3; $i++) { 
+       
+       echo $produtos[$i]['nome'];
+       echo $produtos[$i]['preco'];
+       echo '<img src="./content/'.$produtos[$i]['imagem'].'">';
+       echo '<hr>';
+   }   
+
+
+?>
 
 <?php
 // include do footer
